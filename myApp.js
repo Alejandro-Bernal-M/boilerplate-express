bc788b804410c6f28319369f6df3f58c6e3037bb
alejandro <alejandrober96@gmail.com>
2023-09-29 09:42:17 -0500

    Add app.get with res.sendFile to a view

--- myApp.js
+++ myApp.js
@@ -1,13 +1,20 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
+const path = __dirname + "/views/index.html"
 
 function handler(req, res){
   res.send("Hello Express");
 }
 
+function handler2(req, res){
+  res.sendFile(path);
+}
 
-app.get("/", handler);
+
+// app.get("/", handler);
+
+app.get("/", handler2)
 
 
 
