455a627c22b836e4162d27e3d14110ec1aac73af
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-10-02 11:02:29 -0500

    Add body parser to get data from post request

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 let express = require('express');
+const bodyParser = require("body-parser");
 let app = express();
 console.log("Hello World");
 const path = __dirname + "/views/index.html";
@@ -12,7 +13,8 @@ function handler2(req, res){
   res.sendFile(path);
 }
 
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // app.get("/", handler);
 
 // root middleware function
@@ -53,6 +55,20 @@ app.get(
   "/:word/echo",
   (req,res) => {
     res.json({"echo": req.params.word})
+  }
+)
+
+app.route("/name")
+  .get(
+    (req,res) => {
+      let fullName = `${req.query.first} ${req.query.last}`
+      res.json({"name" : fullName})
+    }
+  )
+  .post(
+    (req, res) => {
+      let fullName = `${req.body.first} ${req.body.last}`
+      res.json({"name" : fullName})
   }
 )
 
